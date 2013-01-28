c25f529668586069954a50e5268135568c21e455
caiovgb@gmail.com <caiovgb@gmail.com@47e1c5ab-ced7-8464-966d-6b5d02211908>
2013-01-28 07:30:39 +0000

    Cleaning up

--- trunk/FlockingExperiments/src/controller/MainController.java
+++ trunk/FlockingExperiments/src/controller/MainController.java
@@ -14,12 +14,12 @@ public class MainController {
 
 		// Constants I believe their optimal values depend on the number of cities
 		int n = 30;
-		int m = n / 2 + 1; // quorum
-		float e = 0.7f; // quorum
+		int m = n / 2 + 1; 
+		float e = 0.7f;
 
 		// Constants I believe do not depend on the number of cities
-		float B = 1; // was 2 before the tests
-		float a = 1f; // not changed (variance tests didn't have significant impact)
+		float B = 1; 
+		float a = 1f;
 
 		TraditionalGraph graph = GenerateSparseInstance.GenerateSparseGraph(n); // Sparse
 		// TraditionalGraph graph = GenerateInstance.GenerateFullyConnectedGraph(n); // Fully connected
