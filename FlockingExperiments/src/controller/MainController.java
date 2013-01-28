0eb1620717a99cfa7d19d847322d827604d34112
caiovgb@gmail.com <caiovgb@gmail.com@47e1c5ab-ced7-8464-966d-6b5d02211908>
2013-01-28 07:26:19 +0000

    Main controllers

---
+++ FlockingExperiments/src/controller/MainController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import graph.FlockingGraph;
+import graph.TraditionalGraph;
+
+/**
+ * TODO Put here a description of what this class does.
+ * 
+ * @author Balthazar. Created Jan 28, 2013.
+ */
+public class MainController {
+	public static void main(String[] args) {
+		int maxT = 5000; // Max iterations
+
+		// Constants I believe their optimal values depend on the number of cities
+		int n = 30;
+		int m = n / 2 + 1; // quorum
+		float e = 0.7f; // quorum
+
+		// Constants I believe do not depend on the number of cities
+		float B = 1; // was 2 before the tests
+		float a = 1f; // not changed (variance tests didn't have significant impact)
+
+		TraditionalGraph graph = GenerateSparseInstance.GenerateSparseGraph(n); // Sparse
+		// BackingGraph graph = GenerateInstance.GenerateFullyConnectedGraph(n); // Fully connected
+
+		long startTime = System.nanoTime();
+		Problem problem = new Problem(new FlockingGraph(graph), maxT);
+		System.out.println(problem.solve(m, e, B, a));
+		long estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("Execution Time: " + estimatedTime / 1000000000.0f + " seconds.");
+
+	}
+}
