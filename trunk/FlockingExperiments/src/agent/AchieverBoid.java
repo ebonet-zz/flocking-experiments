d5391c5cd559990464d00cf95bd8d693e1db3faf
caiovgb@gmail.com <caiovgb@gmail.com@47e1c5ab-ced7-8464-966d-6b5d02211908>
2013-02-04 06:42:19 +0000

    Fixing movement constraints

--- trunk/FlockingExperiments/src/agent/AchieverBoid.java
+++ trunk/FlockingExperiments/src/agent/AchieverBoid.java
@@ -1,10 +1,10 @@
 package agent;
 
-import java.awt.Color;
-import java.util.HashSet;
-
 import graph.Position;
 import graph.Tour;
+
+import java.awt.Color;
+import java.util.HashSet;
 import java.util.Set;
 
 public class AchieverBoid extends Boid {
@@ -17,65 +17,61 @@ public class AchieverBoid extends Boid {
 		this.pathToFollow = new Tour(this.pathTaken);
 		this.pathToFollow.calculateCost(this.graph);
 	}
-	
+
 	@Override
 	public void tryToMove(double distance) {
-		
+
 		// 1) Check for other achiever boids around
 		Set<Boid> boidsInSight = getBoidsInSight();
-		
+
 		// 2) Compare the traveled distance and 3) Update the path
-		for (Boid boid:boidsInSight){
-			
-			if(boid.traveledDistance < this.traveledDistance){
+		for (Boid boid : boidsInSight) {
+
+			if (boid.traveledDistance < this.traveledDistance) {
 				this.pathTaken = new Tour(boid.pathTaken);
 			}
-			
+
 		}
-		
-		
+
 		// 4) Move as before
-				
+
 		super.tryToMove(distance);
 	}
 
-	
 	private Set<Boid> getBoidsInSight() {
 
 		Set<Boid> boidsInSight = new HashSet<>();
-		
-		for (Boid b : environment.getAllBoids()){
-			
-			if(b.isInSight(this)){
+
+		for (Boid b : environment.getAllBoids()) {
+
+			if (b.isInSight(this)) {
 				boidsInSight.add(b);
-			}			
+			}
 		}
-		
+
 		System.out.println(boidsInSight.size());
-		
+
 		return boidsInSight;
 	}
-	
-	
-	
-	public boolean isInSight(Boid b){
-		return b.pos.isSameEdge(pos) && (Math.abs(b.getPos().getDistance()-pos.getDistance())<b.visionRange);
+
+	public boolean isInSight(Boid b) {
+		return b.pos.isSameEdge(pos) && (Math.abs(b.getPos().getDistance() - pos.getDistance()) < b.visionRange);
 	}
-	
 
 	public Tour getPathToFollow() {
 		return this.pathToFollow;
 	}
-	
+
 	public void setPathToFollow(Tour newPath) {
 		// what if path changes mid-move?
 	}
-	
+
 	@Override
 	public void decide() {
-		this.pathTaken.offer(this.getPos().edge.getTo());
+		if (this.pathTaken.lastLocation() != this.pos.edge.getTo())
+			this.pathTaken.offer(this.getPos().edge.getTo());
 
-		if(this.goalEvaluator.isGoal(this.graph, this.pathTaken)){
+		if (this.goalEvaluator.isGoal(this.graph, this.pathTaken)) {
 			respawn();
 			return;
 		}
@@ -86,14 +82,14 @@ public class AchieverBoid extends Boid {
 
 		moveToNextEdge(loadEdge(currentNode, nextNode));
 	}
-	
+
 	public void respawn() {
 		System.out.println(this.pathToFollow.toString());
 		this.pathTaken.clear();
 		this.pathTaken.offer(this.pathToFollow.get(0));
 		this.setPosition(new Position(loadEdge(this.pathToFollow.get(0), this.pathToFollow.get(1)), 0d));
 	}
-	
+
 	@Override
 	public Color getColor() {
 		return Color.blue;
