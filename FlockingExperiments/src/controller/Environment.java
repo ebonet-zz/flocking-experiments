4c3a0b8c1653914cc10cdeea17454ba42bcb7a50
cherryl2007@gmail.com <cherryl2007@gmail.com@47e1c5ab-ced7-8464-966d-6b5d02211908>
2013-02-04 07:39:01 +0000

    Flocks merging is now done in the Environment

--- FlockingExperiments/src/controller/Environment.java
+++ FlockingExperiments/src/controller/Environment.java
@@ -48,9 +48,20 @@ public class Environment {
 		this.flocks.add(flock);
 	}
 	
+	public void joinFlocks(Flock flock1, Flock flock2) {
+		if (flock1.getPathLength() > flock2.getPathLength()) {
+			flock1.acceptOtherFlock(flock2);
+			// TODO: Remove flock2 from map and update number of boids on flock1's path
+		} else {
+			flock2.acceptOtherFlock(flock1);
+			// TODO: Remove flock1 from map and update number of boids on flock2's path
+		}
+	}
+	
 	public void flockDisintegrated(Flock flock) {
 		this.flocks.remove(flock);
 		// TODO: remove this flock's path from the Map of Tours
+		System.out.println("Flock joined another flock");
 	}
 	
 	public Set<Boid> getAllBoids() {
@@ -61,6 +72,19 @@ public class Environment {
 		}
 		
 		return allBoids;
+	}
+	
+	public Set<AchieverBoid> getAllAchievers() {
+		Set<AchieverBoid> achievers = new HashSet<>();
+		for (Flock flock : this.flocks) {
+			achievers.addAll(flock.getBoidsInFlock());
+		}
+		
+		return achievers;
+	}
+	
+	public Set<Boid> getFreeBoids() {
+		return this.freeBoids;
 	}
 	
 	public int getNumberOfFlocks() {
